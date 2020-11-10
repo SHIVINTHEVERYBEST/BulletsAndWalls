3fa962dd9475750e64a27a8d44d926b6cfa1353f
SHIVINTHEVERYBEST <72973429+SHIVINTHEVERYBEST@users.noreply.github.com>
2020-11-10 13:17:24 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,54 @@
+var wall, thickness;
+
+var bullet, speed, weight;
+
+thinkness = random(22, 83);
+
+function setup() {
+  createCanvas(1600,400);
+  
+  createSprite(400, 200, 50, 50);
+
+  car = createSprite(50,200,50,50);
+  wall = createSprite(1200, 200, thickness, height/2);
+  wall.shapeColor = color(80,80,80);
+
+  speed = random(223,321);
+  weight = random(30, 52);
+
+}
+
+function draw() {
+  background(255,255,255); 
+
+  car.velocityX = speed;
+
+  if(hasCollided(lbullet, lwall)){
+
+    bullet.velocityX = 0;
+    var damage = 0.5 * weight * speed * speed/(thickness * thickness * thickness);
+
+    if(damage > 10){
+      wall.shapeColor = color(255,0, 0);
+    }
+    if(damage < 10){
+      wall.shapeColor = color(0, 255, 0);
+    }
+  }
+
+  drawSprites();
+}
+
+funcion (bullet, wall)
+{
+
+
+  bulletRightEdge = lbullet.x + lbullet.width;
+  wallLeftEdge = lwall.x;
+
+  if(bulletRightEdge >= wallLeftEdge){
+    return true
+
+  }
+  return false;
+}
